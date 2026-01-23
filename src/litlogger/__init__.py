bb1a9aa95ee7790588c49bfa16a60d287c26e51c
Justus Schock <12886177+justusschock@users.noreply.github.com>
2026-01-23 12:28:09 +0100

    version bump to 0.1.7

--- src/litlogger/__init__.py
+++ src/litlogger/__init__.py
@@ -18,7 +18,7 @@ For guides and examples, see https://lightning.ai.
 For reference documentation, see https://github.com/Lightning-AI/litlogger.
 """
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 # Import core classes
 # Import preinit utilities
